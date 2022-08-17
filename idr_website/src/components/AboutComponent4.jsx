78406b9d03636e9d3edb3d4f6a1c7b16e9f100ea
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2022-08-17 15:19:38 +0530

    Update AboutComponent4.jsx

--- idr_website/src/components/AboutComponent4.jsx
+++ idr_website/src/components/AboutComponent4.jsx
@@ -17,17 +17,19 @@ justify-content:space-between;
 
 `
 const Title =styled.h1`
-// color:white;
-// color: linear-gradient(186deg, rgba(144,155,132,1) 9%, rgba(138,149,123,1) 14%, rgba(106,115,77,1) 40%, rgba(75,83,32,1) 65%);
+
 background: linear-gradient(186deg, rgba(144,155,132,1) 9%, rgba(138,149,123,1) 14%, rgba(106,115,77,1) 40%, rgba(75,83,32,1) 65%);
 ;
 	-webkit-background-clip: text;
 	-webkit-text-fill-color: transparent;
+
 display:flex;
 justify-content:center;
 align-items:center;
 margin:50px 0px;
 font-size:48px;
+${mobile6({fontSize:"40px", fontWeight:"500"})}
+${mobile3({fontSize:"30px", fontWeight:"500"})}
 `
 // const Image = styled.img``
 
